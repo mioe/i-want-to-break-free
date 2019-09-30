8922e8612c06a6fd1edc1e8ff03489d8ab1dfcd7
mioe <mioe.boss@gmail.com>
2019-09-30 19:35:37 +0300

    add sass to frondend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         frontend-nuxt-js
       </h1>
@@ -28,45 +27,9 @@
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<style scoped lang="sass">
+@import '@/assets/sass/_variables'
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+.container
+  background: $red
 </style>
