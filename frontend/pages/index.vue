30d3ebd69990a5d45d6319c61a5e2791dec18b07
mioe <mioe.boss@gmail.com>
2019-09-30 20:26:02 +0300

    add pug to frontend

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,30 +1,6 @@
-<template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        frontend-nuxt-js
-      </h1>
-      <h2 class="subtitle">
-        My funkadelic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+.view-main
+  h1 frontend-nuxt-js
 </template>
 
 <style scoped lang="sass">
