dba5e7bc7bd30f0e70a766241c1aba70e88917e7
mioe <mioe.boss@gmail.com>
2019-10-01 03:13:38 +0300

    test backend

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,90 @@
 package main
+
 import (
-  "fmt"
   "log"
   "net/http"
+  "math/rand"
+  "strconv"
+  "encoding/json"
   "github.com/gorilla/mux"
 )
-func main() {
-  router := mux.NewRouter()
-  router.HandleFunc("/", DoHealthCheck).Methods("GET")
-  log.Fatal(http.ListenAndServe(":8080", router))
+
+type Book struct {
+  Id string `json:"id"`
+  Title string `json:"title"`
+  Author *Author `json:"author"`
 }
-func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, i'm a golang microservice")
-  w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
+
+type Author struct {
+  Firstname string `json:"firstname"`
+  Lastname string `json:"lastname"`
+}
+
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for _, item := range books {
+     if item.Id == params["id"] {
+      json.NewEncoder(w).Encode(item)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  var book Book
+  _ = json.NewDecoder(r.Body).Decode(&book)
+  book.Id = strconv.Itoa(rand.Intn(1000000))
+  books = append(books, book)
+  json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range books {
+    if item.Id == params["id"] {
+      books = append(books[:index], books[index+1:]...)
+      var book Book
+      _ = json.NewDecoder(r.Body).Decode(&book)
+      book.Id = params["id"]
+      books = append(books, book)
+      json.NewEncoder(w).Encode(book)
+      return
+    }
+  }
+  json.NewEncoder(w).Encode(books)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  params := mux.Vars(r)
+  for index, item := range books {
+    if item.Id == params["id"] {
+      books = append(books[:index], books[index+1:]...)
+      break
+    }
+  }
+  json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+  r := mux.NewRouter()
+  books = append(books, Book{Id: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+  books = append(books, Book{Id: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+  r.HandleFunc("/books", getBooks).Methods("GET")
+  r.HandleFunc("/book/{id}", getBook).Methods("GET")
+  r.HandleFunc("/book", createBook).Methods("POST")
+  r.HandleFunc("/book/{id}", updateBook).Methods("PUT")
+  r.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
+  log.Fatal(http.ListenAndServe(":8080", r))
 }
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,30 @@
 <template lang="pug">
 .view-main
   h1 frontend-nuxt-js
+  ul
+    li(v-for="l in list" :key="l.id")
+      h2 {{ l.title }}
+      p {{ l.author }}
+
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      list: null
+    }
+  },
+
+  async mounted() {
+    await axios.get('/api/books').then(response => {
+      this.list = response.data
+    })
+  }
+}
+</script>
 
 <style scoped lang="sass">
 @import '@/assets/sass/_variables'
